db49392cf5d5b3dbf80dbb9d8ba47f403637695e
AparecidoHonorato <SEU_EMAIL_DO_GITHUB>
2026-01-15 18:39:43 -0300

    ultimo do dia

--- Estetica/server.js
+++ Estetica/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Define a pasta pública (HTML e CSS)
+// Pasta pública (site)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rota principal /
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Inicia o servidor
+// Servidor
 app.listen(PORT, () => {
     console.log(`Site da clínica rodando em http://localhost:${PORT}`);
 });
